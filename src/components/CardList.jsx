b42c38927146bfb1763d342bdc9959bd3b489e76
cj0003 <christyj150@gmail.com>
2024-11-08 20:19:08 +0100

    created Card element for the individual user

--- src/components/CardList.jsx
+++ src/components/CardList.jsx
@@ -1,20 +1,17 @@
 
 import React from 'react';
 
-const Card = ({ userName, todos }) => {
+function Card({ userName, todos }) {
     return (
         <div className="card bg-white dib p-4 grow shadow-md rounded-lg">
             <h2 className="font-bold text-lg mb-2">{userName}'s To-Do List</h2>
-            <div>
-            {console.log(userName)}
             <ul>
-                {todos.map((task, index) => (
-                    <li key={index} className="py-1">
-                        {task}
+                {todos.map((todo) => (
+                    <li key={todo.id} className="border-b border-gray-200 py-1">
+                        {todo.text}
                     </li>
                 ))}
             </ul>
-            </div>
         </div>
     );  
 }   
