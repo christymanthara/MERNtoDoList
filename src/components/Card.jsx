5a07724ed8e19488920c9e0fd6bb9e8306eb456f
cj0003 <christyj150@gmail.com>
2024-11-08 21:37:31 +0100

    todo Cards added

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,14 +3,14 @@ import React from 'react';
 
 const Card = ({ userName, todos }) => {
     return (
-        <div className="card bg-white dib p-4 grow shadow-md rounded-lg">
+        <div className="bg-inherit p-4 shadow-md rounded-lg w-full">
             <h2 className="font-bold text-lg mb-2">{userName}'s To-Do List</h2>
             <div>
             {console.log(userName)}
             <ul>
-                {todos.map((task, index) => (
-                    <li key={index} className="py-1">
-                        {task}
+                {todos.map((todo) => (
+                    <li key={todo.id} className="border-b border-gray-200 py-1">
+                        {todo.text}
                     </li>
                 ))}
             </ul>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,8 @@ import { Link } from 'react-router-dom';
 import { AuthContext } from '../context/AuthContext';
 import '../App.css';
 import axios from 'axios';
+import todosData from '../../src/assets/sampleTodos.json'; //sample todo for the checks
+import Card from '../components/Card';
 
 const apiCall = () => {
   axios.get('http://localhost:5000/')
@@ -25,14 +27,24 @@ function Home() {
 
     return (
         <section className="bg-white dark:bg-gray-900">
-        <div className="lg:grid lg:min-h-screen lg:grid-cols-2">
+        <div className="grid min-h-screen grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
         <section className="relative flex h-32 items-end bg-gray-900 lg:col-span-2 lg:h-full xl:col-span-6">
       <img
         alt=""
         src="https://images.unsplash.com/photo-1617195737496-bc30194e3a19?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
-        className="absolute inset-0 h-full w-full object-cover opacity-80"
+        className="absolute inset-0 h-auto w-full object-cover opacity-80"
       />
-            <div className="hidden lg:relative lg:block lg:p-12">
+      <div className="hidden lg:relative lg:block lg:p-12">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 overflow-auto h-full"> 
+        {todosData.map((user, index) => (
+                    <Card 
+                    key={index} 
+                    userName={user.userName} 
+                    todos={user.todos} />
+                ))}
+
+        </div>
+
         <a className="block text-white" href="#">
           <span className="sr-only">Home</span>
         </a>
@@ -40,9 +52,6 @@ function Home() {
         <h2 className="mt-6 text-2xl font-bold text-white sm:text-3xl md:text-4xl">
         Welcome to the To-Do List 🤹‍♂️
         </h2>
-        <p>
-        apiCall
-        </p>
               
         <p className="mt-4 leading-relaxed text-white/90">
           Your personal companion to organize your stuff on the go!
